ee10f1a99f8cb138b431acdcf713a19287d93370
peeranat009 <159885131+peeranat009@users.noreply.github.com>
2025-03-27 16:51:47 +0700

    update img

--- src/components/projectsSection/ProjectsMain.jsx
+++ src/components/projectsSection/ProjectsMain.jsx
@@ -8,7 +8,7 @@ const projects=[
     name:'Smart bottle recycling cabinet',
     year:'2024',
     align:'right',
-    image:'../../public/images/picproject.jpg',
+    image:'/images/picproject.jpg',
     link:"/videos/videoproject.mp4"
     }
 ]
