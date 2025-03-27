9472ed4eca3c5eb4e8f2de72296c47b9762cb67d
peeranat009 <159885131+peeranat009@users.noreply.github.com>
2025-03-27 22:50:57 +0700

    update button about

--- src/components/aboutMeSection/AboutMeText.jsx
+++ src/components/aboutMeSection/AboutMeText.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-scroll";
+import { FaGithub } from "react-icons/fa";
 function DownloadButton({ fileUrl, fileName }) {
   const openFile = () => {
     window.open(fileUrl, "_blank");
@@ -8,7 +9,10 @@ function DownloadButton({ fileUrl, fileName }) {
           onClick={openFile}
           className="border border-orange rounded-full py-2 px-4 text-lg flex items-center mt-10 hover:bg-orange transition-all duration-500 cursor-pointer md:self-start sm:self-center text-white hover:text-cyan"
       >
-          Download {fileName}
+          {fileName === "github" ? fileName : `Download ${fileName}`}
+          {fileName === "github" ? <>&nbsp;<FaGithub/></> : ""}
+    
+         
       </button>
   );
 }
@@ -18,7 +22,7 @@ const AboutMeText = () => {
       <h2 className='text-6xl text-cyan mb-10'>About Me</h2>
       <p className='text-white text-xl'>&emsp;&emsp;I am a student with a strong passion for coding and problemsolving. I have experience <br/>with programming languages like Java,
         Python, and JavaScript, and am eager to apply <br/>my skills in realworld scenarios. I enjoy working in teams, continuously learning,
-        and improving <br/>my programming abilities. I am looking for an
+        and improving my programming abilities. I am looking for an
         internship where I can grow professionally and contribute to
         exciting and impactful projects.
       </p>
@@ -28,6 +32,7 @@ const AboutMeText = () => {
         </Link>
         <DownloadButton fileUrl="/files/Resume.pdf" fileName="Resume" />
         <DownloadButton fileUrl="/files/Transcript.pdf" fileName="Transcript" />
+        <DownloadButton fileUrl="https://github.com/nine009yu?tab=repositories" fileName="github" />
       </div>
     </div>
   )
